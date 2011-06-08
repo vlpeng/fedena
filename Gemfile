006e7f87ff447d6fd15e9cbce2a52ccd4134c6b5
Chon Doan <domich.dt@gmail.com>
2011-06-08 12:15:24 +0700

    add bundle

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'http://rubygems.org'
+
+gem "mysql"
+gem "rails", "2.3.5"
+gem 'rake', '0.8.7'
+gem 'declarative_authorization'
+gem 'searchlogic'
+gem 'prawn' , '0.6.3'
+group :development do
+  # bundler requires these gems in development
+  # gem "rails-footnotes"
+end
+
+group :test do
+  # bundler requires these gems while running tests
+  # gem "rspec"
+  # gem "faker"
+end
--- config/environment.rb
+++ config/environment.rb
@@ -5,11 +5,6 @@ RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 Rails::Initializer.run do |config|
   config.time_zone = 'UTC'
-
-  config.gem 'declarative_authorization', :source => 'http://gemcutter.org'
-  config.gem 'searchlogic'
-  config.gem 'prawn'
-
   config.load_once_paths += %W( #{RAILS_ROOT}/lib )
   config.load_paths += Dir["#{RAILS_ROOT}/app/models/*"].find_all { |f| File.stat(f).directory? }
 end
